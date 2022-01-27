e72de126b34bf3c61259e0605227f5dc2d60dedd
Abu Anwar <abuanwar072@gmail.com>
2022-01-27 19:15:21 +0600

    refector

---
+++ lib/screens/details/components/color_dot.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+
+class ColorDot extends StatelessWidget {
+  const ColorDot({
+    Key? key,
+    required this.color,
+    required this.isActive,
+  }) : super(key: key);
+
+  final Color color;
+  final bool isActive;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(defaultPadding / 4),
+      decoration: BoxDecoration(
+        border: Border.all(color: isActive ? primaryColor : Colors.transparent),
+        shape: BoxShape.circle,
+      ),
+      child: CircleAvatar(
+        radius: 10,
+        backgroundColor: color,
+      ),
+    );
+  }
+}
--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter_svg/svg.dart';
 import 'package:stylish/constants.dart';
 import 'package:stylish/models/Product.dart';
 
+import 'components/color_dot.dart';
+
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({Key? key, required this.product}) : super(key: key);
 
@@ -110,32 +112,6 @@ class DetailsScreen extends StatelessWidget {
             ),
           )
         ],
-      ),
-    );
-  }
-}
-
-class ColorDot extends StatelessWidget {
-  const ColorDot({
-    Key? key,
-    required this.color,
-    required this.isActive,
-  }) : super(key: key);
-
-  final Color color;
-  final bool isActive;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(defaultPadding / 4),
-      decoration: BoxDecoration(
-        border: Border.all(color: isActive ? primaryColor : Colors.transparent),
-        shape: BoxShape.circle,
-      ),
-      child: CircleAvatar(
-        radius: 10,
-        backgroundColor: color,
       ),
     );
   }
