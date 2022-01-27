f6d89a2d53abb1783d5e6dfdcc91c0aacb6a2620
Abu Anwar <abuanwar072@gmail.com>
2022-01-27 19:16:25 +0600

    product added

---
+++ lib/models/Product.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Product {
+  final String image, title;
+  final int price;
+  final Color bgColor;
+
+  Product({
+    required this.image,
+    required this.title,
+    required this.price,
+    this.bgColor = const Color(0xFFEFEFF2),
+  });
+}
+
+List<Product> demo_product = [
+  Product(
+    image: "assets/images/product_0.png",
+    title: "Long Sleeve Shirts",
+    price: 165,
+    bgColor: const Color(0xFFFEFBF9),
+  ),
+  Product(
+    image: "assets/images/product_1.png",
+    title: "Casual Henley Shirts",
+    price: 99,
+  ),
+  Product(
+    image: "assets/images/product_2.png",
+    title: "Curved Hem Shirts",
+    price: 180,
+    bgColor: const Color(0xFFF8FEFB),
+  ),
+  Product(
+    image: "assets/images/product_3.png",
+    title: "Casual Nolin",
+    price: 149,
+    bgColor: const Color(0xFFEEEEED),
+  ),
+];
