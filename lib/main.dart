4fec7cfe4bdcc52db8f4d0d9456848663b35e2ec
Abu Anwar <abuanwar072@gmail.com>
2022-01-27 10:32:20 +0600

    appbar done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stylish/constants.dart';
 import 'package:stylish/screens/home/home_screen.dart';
 
 void main() {
@@ -15,8 +16,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'The Flutter Way',
       theme: ThemeData(
+        scaffoldBackgroundColor: bgColor,
         primarySwatch: Colors.blue,
         fontFamily: "Gordita",
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
         textTheme: const TextTheme(
           bodyText2: TextStyle(color: Colors.black54),
         ),
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:stylish/constants.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset("assets/icons/menu.svg"),
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset("assets/icons/Location.svg"),
+            const SizedBox(width: defaultPadding / 2),
+            Text(
+              "15/2 New Texas",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            icon: SvgPicture.asset("assets/icons/Notification.svg"),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
