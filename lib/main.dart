a2b22fc8b41a7224dd19853bd923c80420394dd5
Abu Anwar <abuanwar072@gmail.com>
2022-01-27 09:27:48 +0600

    starting

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'The Flutter Way',
       theme: ThemeData(
         primarySwatch: Colors.blue,
